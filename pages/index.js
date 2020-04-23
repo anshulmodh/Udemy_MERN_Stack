4c80e7e9c26e535f9b1fee95a81fe4ffcc97a6aa
anshulmodh <35407665+anshulmodh@users.noreply.github.com>
2020-04-22 21:06:40 -0500

    mongo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-function Home() {
-  return <>home</>;
+import React from 'react'
+import axios from 'axios'
+import ProductList from '../components/Index/ProductList'
+
+function Home({ products }) {
+  return <ProductList products={products} />;
+}
+
+Home.getInitialProps = async () => {
+  // fetch data on the server
+  const url = 'http://localhost:3000/api/products';
+    const response = await axios.get(url);
+  // return response data as an object
+  return { products: response.data };
+  // note: this object will be merged with existing props
+
 }
 
 export default Home;
